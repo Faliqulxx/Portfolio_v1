862dd87e238a3cf7e8247b5f870cc9a2b94d50bc
Faliqulxx <faliqul.isback@gmail.com>
2025-12-28 20:02:57 +0700

    add

--- components/intro.tsx
+++ components/intro.tsx
@@ -95,7 +95,7 @@ learning workflows.
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/cv/resume.pdf"
+          href="/cv/resumev1.pdf"
           download
         >
           Download CV{" "}
