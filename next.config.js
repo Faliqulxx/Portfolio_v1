9a153f8fe059ddc6827a616384660b8feabf6008
Faliqulxx <faliqul.isback@gmail.com>
2026-07-28 16:01:38 +0700

    Chore: Add asyncWebAssembly webpack config in next.config.js for Rapier physics

--- next.config.js
+++ next.config.js
@@ -11,5 +11,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 module.exports = nextConfig;
+
